09530d267e9fdd16ea3c39f962265b0c61305852
ansarkhan <ansarkhan@me.com>
2019-03-18 07:35:46 -0400

    db prepped

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Lucky Buns Burger", FALSE);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Dukes Burger", TRUE);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("In N Out Burger", TRUE);
