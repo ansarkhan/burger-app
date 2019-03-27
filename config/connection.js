75d1cb9b4de98b831c196d81b1e30069ce0b2e49
ansarkhan <ansarkhan@me.com>
2019-03-27 09:20:57 -0400

    added Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    database: "burgers_db",
-    user: "root",
-    password: "password"
-});
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        database: "burgers_db",
+        user: "root",
+        password: "password"
+    });
+
+}
+
 
 connection.connect(function(err) {
     if (err) throw err;
